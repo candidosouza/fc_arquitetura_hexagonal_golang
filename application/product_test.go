495c4078797fbe710972c95bb54ad6282becb1cc
Candido Souza <candidosouzza@gmail.com>
2021-08-09 09:36:56 -0300

    Testes de erro no enable

--- application/product_test.go
+++ application/product_test.go
@@ -1,7 +1,7 @@
 package application_test
 
 import (
-	"github.com/candidosouza/fc_arquitetura_hexagonal/application"
+	"github.com/candidosouza/fc_arquitetura_hexagonal_golang/application"
 	"testing"
 	"github.com/stretchr/testify/require"
 )
@@ -14,4 +14,9 @@ func TestProduct_Enable(t *testing.T) {
 
 	err := product.Enable()
 	require.Nil(t, error)
+
+	product.Price = 0
+
+	err := product.Enable()
+	require.Equal(t, "The price must be greater than zero to enable the product", err.Error())
 }
